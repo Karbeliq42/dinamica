4802dd93d7fe54629103f0ebf60195e717c75d0c
Karbeliq42 <fabio13caixeta@gmail.com>
2023-07-04 22:41:19 -0300

    foiii

--- dinamica.c
+++ dinamica.c
@@ -1,36 +1,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
-
-#ifdef _WIN32
-#define CLEAR_SCREEN "cls"
-#else
-#define CLEAR_SCREEN "clear"
-#endif
-
-#define MaxUsers 3
 
 typedef struct {
   int id;
-  char nome[20];
+  char nome[50];
+  char sexo[20];
   double altura;
   int vacinado;
   char email[50];
-  char sexo[15];
 } Usuario;
 
 void cadastrarUsuario(Usuario **usuarios, int *numUsuarios, int *contadorIDs) {
-  if (*numUsuarios >= MaxUsers) {
-    printf("Número máximo de usuários atingido.\n");
-    return;
-  }
-
   Usuario novoUsuario;
 
-  novoUsuario.id = (*contadorIDs)++;
-  printf("ID gerado: %d\n", novoUsuario.id);
-
+  printf("----- Cadastrar Usuário -----\n");
   printf("Digite o nome: ");
   scanf("%s", novoUsuario.nome);
 
@@ -38,12 +22,16 @@ void cadastrarUsuario(Usuario **usuarios, int *numUsuarios, int *contadorIDs) {
     printf("Digite o sexo (feminino, masculino ou indiferente): ");
     scanf("%s", novoUsuario.sexo);
 
-    if (strcmp(novoUsuario.sexo, "feminino") == 0 || strcmp(novoUsuario.sexo, "masculino") == 0 || strcmp(novoUsuario.sexo, "indiferente") == 0) {
+    if (strcmp(novoUsuario.sexo, "feminino") == 0 ||
+        strcmp(novoUsuario.sexo, "masculino") == 0 ||
+        strcmp(novoUsuario.sexo, "indiferente") == 0) {
       printf("Sexo válido.\n");
     } else {
       printf("Sexo inválido. Digite novamente.\n");
     }
-  } while (!(strcmp(novoUsuario.sexo, "feminino") == 0 || strcmp(novoUsuario.sexo, "masculino") == 0 || strcmp(novoUsuario.sexo, "indiferente") == 0));
+  } while (!(strcmp(novoUsuario.sexo, "feminino") == 0 ||
+             strcmp(novoUsuario.sexo, "masculino") == 0 ||
+             strcmp(novoUsuario.sexo, "indiferente") == 0));
 
   do {
     printf("Digite a altura: ");
@@ -86,6 +74,7 @@ void cadastrarUsuario(Usuario **usuarios, int *numUsuarios, int *contadorIDs) {
     }
   } while (1);
 
+  novoUsuario.id = (*contadorIDs)++;
   (*numUsuarios)++;
   *usuarios = realloc(*usuarios, (*numUsuarios) * sizeof(Usuario));
   (*usuarios)[(*numUsuarios) - 1] = novoUsuario;
@@ -99,26 +88,29 @@ void editarUsuario(Usuario *usuarios, int numUsuarios) {
   scanf("%d", &idEditar);
 
   for (int i = 0; i < numUsuarios; i++) {
-    if (idEditar == usuarios[i].id) {
+    if (usuarios[i].id == idEditar) {
       printf("----- Editar Usuário -----\n");
-      printf("ID: %d\n", usuarios[i].id);
 
-      printf("Digite o nome: ");
+      printf("Digite o novo nome: ");
       scanf("%s", usuarios[i].nome);
 
       do {
-        printf("Digite o sexo (feminino, masculino ou indiferente): ");
+        printf("Digite o novo sexo (feminino, masculino ou indiferente): ");
         scanf("%s", usuarios[i].sexo);
 
-        if (strcmp(usuarios[i].sexo, "feminino") == 0 || strcmp(usuarios[i].sexo, "masculino") == 0 || strcmp(usuarios[i].sexo, "indiferente") == 0) {
+        if (strcmp(usuarios[i].sexo, "feminino") == 0 ||
+            strcmp(usuarios[i].sexo, "masculino") == 0 ||
+            strcmp(usuarios[i].sexo, "indiferente") == 0) {
           printf("Sexo válido.\n");
         } else {
           printf("Sexo inválido. Digite novamente.\n");
         }
-      } while (!(strcmp(usuarios[i].sexo, "feminino") == 0 || strcmp(usuarios[i].sexo, "masculino") == 0 || strcmp(usuarios[i].sexo, "indiferente") == 0));
+      } while (!(strcmp(usuarios[i].sexo, "feminino") == 0 ||
+                 strcmp(usuarios[i].sexo, "masculino") == 0 ||
+                 strcmp(usuarios[i].sexo, "indiferente") == 0));
 
       do {
-        printf("Digite a altura: ");
+        printf("Digite a nova altura: ");
         scanf("%lf", &usuarios[i].altura);
 
         if (usuarios[i].altura >= 1 && usuarios[i].altura <= 2) {
@@ -146,7 +138,7 @@ void editarUsuario(Usuario *usuarios, int numUsuarios) {
       } while (usuarios[i].vacinado != 1 && usuarios[i].vacinado != 2);
 
       do {
-        printf("Digite o seu email: ");
+        printf("Digite o novo email: ");
         scanf("%s", usuarios[i].email);
 
         // Verificar se o caractere '@' está presente no email
@@ -172,9 +164,8 @@ void excluirUsuario(Usuario **usuarios, int *numUsuarios) {
   scanf("%d", &idExcluir);
 
   for (int i = 0; i < *numUsuarios; i++) {
-    if (idExcluir == (*usuarios)[i].id) {
-      // Deslocar os usuários para preencher o espaço do usuário excluído
-      for (int j = i; j < *numUsuarios - 1; j++) {
+    if ((*usuarios)[i].id == idExcluir) {
+      for (int j = i; j < (*numUsuarios) - 1; j++) {
         (*usuarios)[j] = (*usuarios)[j + 1];
       }
 
@@ -190,7 +181,7 @@ void excluirUsuario(Usuario **usuarios, int *numUsuarios) {
 
 void listarUsuarios(Usuario *usuarios, int numUsuarios) {
   if (numUsuarios == 0) {
-    printf("Não há usuários cadastrados.\n");
+    printf("Nenhum usuário cadastrado.\n");
     return;
   }
 
@@ -199,26 +190,26 @@ void listarUsuarios(Usuario *usuarios, int numUsuarios) {
     printf("ID: %d\n", usuarios[i].id);
     printf("Nome: %s\n", usuarios[i].nome);
     printf("Sexo: %s\n", usuarios[i].sexo);
-    printf("Altura: %.2lf\n", usuarios[i].altura);
-    printf("Vacinado: %s\n", usuarios[i].vacinado == 1 ? "Sim" : "Não");
+    printf("Altura: %.2f\n", usuarios[i].altura);
+    printf("Vacinado: %s\n", (usuarios[i].vacinado == 1) ? "Sim" : "Não");
     printf("Email: %s\n", usuarios[i].email);
     printf("-----------------------------\n");
   }
 }
 
 int main() {
-  int opcao;
+  Usuario *usuarios = NULL;
   int numUsuarios = 0;
   int contadorIDs = 1;
-  Usuario *usuarios = NULL;
+  int opcao;
 
-  while (1) {
+  do {
     printf("----- Menu -----\n");
     printf("1 - Cadastrar usuário\n");
     printf("2 - Editar usuário\n");
     printf("3 - Excluir usuário\n");
     printf("4 - Listar usuários\n");
-    printf("5 - Sair\n");
+    printf("0 - Sair\n");
     printf("Escolha uma opção: ");
     scanf("%d", &opcao);
 
@@ -235,14 +226,18 @@ int main() {
     case 4:
       listarUsuarios(usuarios, numUsuarios);
       break;
-    case 5:
-      free(usuarios);
-      return 0;
+    case 0:
+      printf("Encerrando o programa.\n");
+      break;
     default:
       printf("Opção inválida. Digite novamente.\n");
       break;
     }
-  }
+
+    printf("\n");
+  } while (opcao != 0);
+
+  free(usuarios);
 
   return 0;
 }

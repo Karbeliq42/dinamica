0c08dfe8bba54b6c5c5ca06fce50eafdb6c0b0d0
Karbeliq42 <fabio13caixeta@gmail.com>
2023-07-09 23:59:18 -0300

    opa

--- dinamica.c
+++ dinamica.c
@@ -1,243 +1,132 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
+#define STRING_T 60
 
 typedef struct {
   int id;
-  char nome[50];
-  char sexo[20];
-  double altura;
-  int vacinado;
-  char email[50];
-} Usuario;
+  char nome[STRING_T];
+  char email[STRING_T];
+  double Altura;
+  char End[STRING_T];
+} Registro;
 
-void cadastrarUsuario(Usuario **usuarios, int *numUsuarios, int *contadorIDs) {
-  Usuario novoUsuario;
+typedef struct {
+  Registro *registros;
+  int tamanho;
+} ListaRegistros;
 
-  printf("----- Cadastrar Usuário -----\n");
+int proximoID = 1;
+
+ListaRegistros *inicializarLista() {
+  ListaRegistros *lista = (ListaRegistros *)malloc(sizeof(ListaRegistros));
+  lista->registros = NULL;
+  lista->tamanho = 0;
+  return lista;
+}
+int gerarID() {
+  return proximoID++;
+}
+
+void adicionarRegistro(ListaRegistros *lista) {
+  Registro registro;
+
+  registro.id = gerarID();
+  printf("ID: %d \n", registro.id);
+
   printf("Digite o nome: ");
-  scanf("%s", novoUsuario.nome);
+  fgets(registro.nome, STRING_T, stdin);
+  registro.nome[strcspn(registro.nome, "\n")] = '\0';
 
+  int verificar = 0;
   do {
-    printf("Digite o sexo (feminino, masculino ou indiferente): ");
-    scanf("%s", novoUsuario.sexo);
+    printf("Digite seu email: ");
+    fgets(registro.email, STRING_T, stdin);
+    fflush(stdin);
 
-    if (strcmp(novoUsuario.sexo, "feminino") == 0 ||
-        strcmp(novoUsuario.sexo, "masculino") == 0 ||
-        strcmp(novoUsuario.sexo, "indiferente") == 0) {
-      printf("Sexo válido.\n");
+    if (strstr(registro.email, "@") == NULL) {
+      printf("E-mail invalido.\n");
+
     } else {
-      printf("Sexo inválido. Digite novamente.\n");
+      verificar = 1;
     }
-  } while (!(strcmp(novoUsuario.sexo, "feminino") == 0 ||
-             strcmp(novoUsuario.sexo, "masculino") == 0 ||
-             strcmp(novoUsuario.sexo, "indiferente") == 0));
+
+  } while (!verificar);
+
+  printf("Digite o endereco: ");
+  fgets(registro.End, STRING_T, stdin);
+  registro.End[strcspn(registro.End, "\n")] = '\0';
 
   do {
     printf("Digite a altura: ");
-    scanf("%lf", &novoUsuario.altura);
+    scanf("%lf", &registro.Altura);
+    getchar();
 
-    if (novoUsuario.altura >= 1 && novoUsuario.altura <= 2) {
-      printf("Altura válida.\n");
-    } else {
-      printf("Altura inválida. Digite novamente.\n");
-    }
-  } while (!(novoUsuario.altura >= 1 && novoUsuario.altura <= 2));
+    if (registro.Altura < 1 || registro.Altura > 2)
+      printf("Altura invalida \n");
 
-  do {
-    printf("Digite 1 se você estiver vacinado ou 2 se não estiver: ");
-    scanf("%d", &novoUsuario.vacinado);
+  } while (registro.Altura < 1 || registro.Altura > 2);
 
-    switch (novoUsuario.vacinado) {
-    case 1:
-      printf("Você está vacinado!\n");
-      break;
-    case 2:
-      printf("Você não está vacinado.\n");
-      break;
-    default:
-      printf("Opção inválida. Digite novamente.\n");
-      break;
-    }
-  } while (novoUsuario.vacinado != 1 && novoUsuario.vacinado != 2);
-
-  do {
-    printf("Digite o seu email: ");
-    scanf("%s", novoUsuario.email);
-
-    // Verificar se o caractere '@' está presente no email
-    if (strstr(novoUsuario.email, "@") == NULL) {
-      printf("Email inválido. Digite novamente.\n");
-    } else {
-      printf("Email válido.\n");
-      break; // Sai do loop quando o email for válido
-    }
-  } while (1);
-
-  novoUsuario.id = (*contadorIDs)++;
-  (*numUsuarios)++;
-  *usuarios = realloc(*usuarios, (*numUsuarios) * sizeof(Usuario));
-  (*usuarios)[(*numUsuarios) - 1] = novoUsuario;
-
-  printf("Usuário cadastrado com sucesso!\n");
+  lista->registros = (Registro *)realloc(
+      lista->registros, (lista->tamanho + 1) * sizeof(Registro));
+  lista->registros[lista->tamanho] = registro;
+  lista->tamanho++;
 }
 
-void editarUsuario(Usuario *usuarios, int numUsuarios) {
-  int idEditar;
-  printf("Digite o ID do usuário que deseja editar: ");
-  scanf("%d", &idEditar);
 
-  for (int i = 0; i < numUsuarios; i++) {
-    if (usuarios[i].id == idEditar) {
-      printf("----- Editar Usuário -----\n");
+void exibirRegistros(ListaRegistros *lista) {
+  printf("Registros:\n");
 
-      printf("Digite o novo nome: ");
-      scanf("%s", usuarios[i].nome);
-
-      do {
-        printf("Digite o novo sexo (feminino, masculino ou indiferente): ");
-        scanf("%s", usuarios[i].sexo);
-
-        if (strcmp(usuarios[i].sexo, "feminino") == 0 ||
-            strcmp(usuarios[i].sexo, "masculino") == 0 ||
-            strcmp(usuarios[i].sexo, "indiferente") == 0) {
-          printf("Sexo válido.\n");
-        } else {
-          printf("Sexo inválido. Digite novamente.\n");
-        }
-      } while (!(strcmp(usuarios[i].sexo, "feminino") == 0 ||
-                 strcmp(usuarios[i].sexo, "masculino") == 0 ||
-                 strcmp(usuarios[i].sexo, "indiferente") == 0));
-
-      do {
-        printf("Digite a nova altura: ");
-        scanf("%lf", &usuarios[i].altura);
-
-        if (usuarios[i].altura >= 1 && usuarios[i].altura <= 2) {
-          printf("Altura válida.\n");
-        } else {
-          printf("Altura inválida. Digite novamente.\n");
-        }
-      } while (!(usuarios[i].altura >= 1 && usuarios[i].altura <= 2));
-
-      do {
-        printf("Digite 1 se você estiver vacinado ou 2 se não estiver: ");
-        scanf("%d", &usuarios[i].vacinado);
-
-        switch (usuarios[i].vacinado) {
-        case 1:
-          printf("Você está vacinado!\n");
-          break;
-        case 2:
-          printf("Você não está vacinado.\n");
-          break;
-        default:
-          printf("Opção inválida. Digite novamente.\n");
-          break;
-        }
-      } while (usuarios[i].vacinado != 1 && usuarios[i].vacinado != 2);
-
-      do {
-        printf("Digite o novo email: ");
-        scanf("%s", usuarios[i].email);
-
-        // Verificar se o caractere '@' está presente no email
-        if (strstr(usuarios[i].email, "@") == NULL) {
-          printf("Email inválido. Digite novamente.\n");
-        } else {
-          printf("Email válido.\n");
-          break; // Sai do loop quando o email for válido
-        }
-      } while (1);
-
-      printf("Usuário editado com sucesso!\n");
-      return;
-    }
+  for (int i = 0; i < lista->tamanho; i++) {
+    Registro registro = lista->registros[i];
+    printf("ID: %d\n", registro.id);
+    printf("Nome: %s\n", registro.nome);
+    printf("Email: %s\n", registro.email);
+    printf("Altura: %lf\n", registro.Altura);
+    printf("Endereco: %s\n", registro.End);
+    printf("\n");
   }
-
-  printf("Usuário não encontrado.\n");
 }
 
-void excluirUsuario(Usuario **usuarios, int *numUsuarios) {
-  int idExcluir;
-  printf("Digite o ID do usuário que deseja excluir: ");
-  scanf("%d", &idExcluir);
-
-  for (int i = 0; i < *numUsuarios; i++) {
-    if ((*usuarios)[i].id == idExcluir) {
-      for (int j = i; j < (*numUsuarios) - 1; j++) {
-        (*usuarios)[j] = (*usuarios)[j + 1];
-      }
-
-      (*numUsuarios)--;
-      *usuarios = realloc(*usuarios, (*numUsuarios) * sizeof(Usuario));
-      printf("Usuário excluído com sucesso!\n");
-      return;
-    }
-  }
-
-  printf("Usuário não encontrado.\n");
-}
-
-void listarUsuarios(Usuario *usuarios, int numUsuarios) {
-  if (numUsuarios == 0) {
-    printf("Nenhum usuário cadastrado.\n");
-    return;
-  }
-
-  printf("----- Lista de Usuários -----\n");
-  for (int i = 0; i < numUsuarios; i++) {
-    printf("ID: %d\n", usuarios[i].id);
-    printf("Nome: %s\n", usuarios[i].nome);
-    printf("Sexo: %s\n", usuarios[i].sexo);
-    printf("Altura: %.2f\n", usuarios[i].altura);
-    printf("Vacinado: %s\n", (usuarios[i].vacinado == 1) ? "Sim" : "Não");
-    printf("Email: %s\n", usuarios[i].email);
-    printf("-----------------------------\n");
-  }
+void liberarLista(ListaRegistros *lista) {
+  free(lista->registros);
+  free(lista);
 }
 
 int main() {
-  Usuario *usuarios = NULL;
-  int numUsuarios = 0;
-  int contadorIDs = 1;
-  int opcao;
+  ListaRegistros *lista = inicializarLista();
+  char opcao;
 
   do {
-    printf("----- Menu -----\n");
-    printf("1 - Cadastrar usuário\n");
-    printf("2 - Editar usuário\n");
-    printf("3 - Excluir usuário\n");
-    printf("4 - Listar usuários\n");
-    printf("0 - Sair\n");
-    printf("Escolha uma opção: ");
-    scanf("%d", &opcao);
+    printf("=== Agora Vai ===\n");
+    printf("1. Incluir registro\n");
+    printf("2. Listar registros\n");
+    printf("5. Sair\n");
+    printf("Escolha uma opcao: ");
+    scanf(" %c", &opcao);
+    getchar();
 
     switch (opcao) {
-    case 1:
-      cadastrarUsuario(&usuarios, &numUsuarios, &contadorIDs);
+    case '1':
+      adicionarRegistro(lista);
       break;
-    case 2:
-      editarUsuario(usuarios, numUsuarios);
+    case '2':
+      exibirRegistros(lista);
       break;
-    case 3:
-      excluirUsuario(&usuarios, &numUsuarios);
-      break;
-    case 4:
-      listarUsuarios(usuarios, numUsuarios);
-      break;
-    case 0:
-      printf("Encerrando o programa.\n");
+    case '3':
+      printf("Saindo...\n");
       break;
     default:
-      printf("Opção inválida. Digite novamente.\n");
+      printf("Opcao invalida!\n");
       break;
     }
 
     printf("\n");
-  } while (opcao != 0);
+  } while (opcao != '3');
 
-  free(usuarios);
+  liberarLista(lista);
 
   return 0;
 }

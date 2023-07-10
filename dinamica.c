fc71f8a636aeb7a8d985d65c86a2462795848159
Karbeliq42 <fabio13caixeta@gmail.com>
2023-07-10 15:22:41 -0300

    sera?

--- dinamica.c
+++ dinamica.c
@@ -1,16 +1,15 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
-#include <time.h>
 
-#define STRING_T 60
+#define STRING_T 50
 
 typedef struct {
   int id;
-  char nome[STRING_T];
-  char email[STRING_T];
-  double Altura;
-  char End[STRING_T];
+  char *nome;
+  char *email;
+  double altura;
+  char *endereco;
 } Registro;
 
 typedef struct {
@@ -26,9 +25,8 @@ ListaRegistros *inicializarLista() {
   lista->tamanho = 0;
   return lista;
 }
-int gerarID() {
-  return proximoID++;
-}
+
+int gerarID() { return proximoID++; }
 
 void adicionarRegistro(ListaRegistros *lista) {
   Registro registro;
@@ -36,38 +34,39 @@ void adicionarRegistro(ListaRegistros *lista) {
   registro.id = gerarID();
   printf("ID: %d \n", registro.id);
 
+  registro.nome = (char *)malloc(STRING_T * sizeof(char));
   printf("Digite o nome: ");
   fgets(registro.nome, STRING_T, stdin);
   registro.nome[strcspn(registro.nome, "\n")] = '\0';
 
   int verificar = 0;
   do {
+    registro.email = (char *)malloc(STRING_T * sizeof(char));
     printf("Digite seu email: ");
     fgets(registro.email, STRING_T, stdin);
-    fflush(stdin);
 
     if (strstr(registro.email, "@") == NULL) {
-      printf("E-mail invalido.\n");
-
+      printf("E-mail inválido.\n");
+      free(registro.email);
     } else {
       verificar = 1;
     }
-
   } while (!verificar);
 
+  registro.endereco = (char *)malloc(STRING_T * sizeof(char));
   printf("Digite o endereco: ");
-  fgets(registro.End, STRING_T, stdin);
-  registro.End[strcspn(registro.End, "\n")] = '\0';
+  fgets(registro.endereco, STRING_T, stdin);
+  registro.endereco[strcspn(registro.endereco, "\n")] = '\0';
 
   do {
     printf("Digite a altura: ");
-    scanf("%lf", &registro.Altura);
+    scanf("%lf", &registro.altura);
     getchar();
 
-    if (registro.Altura < 1 || registro.Altura > 2)
-      printf("Altura invalida \n");
+    if (registro.altura < 1 || registro.altura > 2)
+      printf("Altura inválida \n");
 
-  } while (registro.Altura < 1 || registro.Altura > 2);
+  } while (registro.altura < 1 || registro.altura > 2);
 
   lista->registros = (Registro *)realloc(
       lista->registros, (lista->tamanho + 1) * sizeof(Registro));
@@ -75,22 +74,122 @@ void adicionarRegistro(ListaRegistros *lista) {
   lista->tamanho++;
 }
 
-
 void exibirRegistros(ListaRegistros *lista) {
   printf("Registros:\n");
 
+  
   for (int i = 0; i < lista->tamanho; i++) {
     Registro registro = lista->registros[i];
     printf("ID: %d\n", registro.id);
     printf("Nome: %s\n", registro.nome);
     printf("Email: %s\n", registro.email);
-    printf("Altura: %lf\n", registro.Altura);
-    printf("Endereco: %s\n", registro.End);
+    printf("Altura: %lf\n", registro.altura);
+    printf("Endereco: %s\n", registro.endereco);
     printf("\n");
   }
 }
 
+void editarRegistros(ListaRegistros *lista) {
+  int id;
+  printf("Digite o ID que quer alterar:");
+  scanf("%d", &id);
+  getchar();
+
+  int encontrado = 0;
+  for (int i = 0; i < lista->tamanho; i++) {
+    if (lista->registros[i].id == id) {
+      Registro *registro = &(lista->registros[i]);
+
+      printf("Registro encontrado:\n");
+      printf("ID: %d\n", registro->id);
+      printf("Nome: %s\n", registro->nome);
+      printf("Email: %s\n", registro->email);
+      printf("Altura: %lf\n", registro->altura);
+      printf("Endereco: %s\n", registro->endereco);
+
+      printf("Digite o novo nome: ");
+      fgets(registro->nome, STRING_T, stdin);
+      registro->nome[strcspn(registro->nome, "\n")] = '\0';
+
+      int verificar = 0;
+      do {
+        printf("Digite o novo email: ");
+        fgets(registro->email, STRING_T, stdin);
+
+        if (strstr(registro->email, "@") == NULL) {
+          printf("E-mail inválido.\n");
+        } else {
+          verificar = 1;
+        }
+      } while (!verificar);
+
+      printf("Digite o novo endereco: ");
+      fgets(registro->endereco, STRING_T, stdin);
+      registro->endereco[strcspn(registro->endereco, "\n")] = '\0';
+
+      do {
+        printf("Digite a nova altura: ");
+        scanf("%lf", &registro->altura);
+        getchar();
+
+        if (registro->altura < 1 || registro->altura > 2)
+          printf("Altura inválida \n");
+
+      } while (registro->altura < 1 || registro->altura > 2);
+
+      printf("Registro alterado com sucesso!\n");
+
+      encontrado = 1;
+      break;
+    }
+  }
+
+  if (!encontrado) {
+    printf("Registro com o ID %d não encontrado.\n", id);
+  }
+}
+
+void excluirRegistro(ListaRegistros *lista) {
+  int id;
+  printf("Digite o ID que quer excluir:");
+  scanf("%d", &id);
+  getchar();
+
+  int encontrado = 0;
+  int indice_encontrado = 0;
+  for (int i = 0; i < lista->tamanho; i++) {
+    if (lista->registros[i].id == id) {
+      encontrado = 1;
+      indice_encontrado = i;
+      break;
+    }
+  }
+  if (encontrado) {
+    Registro registro_excluido = lista->registros[indice_encontrado];
+
+    free(registro_excluido.nome);
+    free(registro_excluido.email);
+    free(registro_excluido.endereco);
+
+    for (int i = indice_encontrado; i < lista->tamanho - 1; i++) {
+      lista->registros[i] = lista->registros[i + 1];
+    }
+
+    lista->tamanho--;
+    lista->registros = (Registro *)realloc(lista->registros, lista->tamanho * sizeof(Registro));
+
+    printf("Registro com ID %d excluido com sucesso.\n", id);
+  } else {
+    printf("Registro com ID %d não encontrado.\n", id);
+  }
+}
+
 void liberarLista(ListaRegistros *lista) {
+  for (int i = 0; i < lista->tamanho; i++) {
+    free(lista->registros[i].nome);
+    free(lista->registros[i].email);
+    free(lista->registros[i].endereco);
+  }
   free(lista->registros);
   free(lista);
 }
@@ -103,28 +202,36 @@ int main() {
     printf("=== Agora Vai ===\n");
     printf("1. Incluir registro\n");
     printf("2. Listar registros\n");
+    printf("3. Editar registros\n");
+    printf("4. Excluir registro\n");
     printf("5. Sair\n");
-    printf("Escolha uma opcao: ");
+    printf("Escolha uma opção: ");
     scanf(" %c", &opcao);
     getchar();
 
     switch (opcao) {
-    case '1':
-      adicionarRegistro(lista);
-      break;
-    case '2':
-      exibirRegistros(lista);
-      break;
-    case '3':
-      printf("Saindo...\n");
-      break;
-    default:
-      printf("Opcao invalida!\n");
-      break;
+      case '1':
+        adicionarRegistro(lista);
+        break;
+      case '2':
+        exibirRegistros(lista);
+        break;
+      case '3':
+        editarRegistros(lista);
+        break;
+      case '4':
+        excluirRegistro(lista);
+        break;
+      case '5':
+        printf("Saindo...\n");
+        break;
+      default:
+        printf("Opção inválida!\n");
+        break;
     }
 
     printf("\n");
-  } while (opcao != '3');
+  } while (opcao != '5');
 
   liberarLista(lista);
 
